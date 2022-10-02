9f136e32a7e31f94541c4738721624be833a890e
user0032 <70772813+PCuser0032@users.noreply.github.com>
2022-10-02 14:38:20 +0300

    Add SignInForm

---
+++ src/components/SignInForm.js
@@ -0,0 +1,38 @@
+import React from "react";
+
+function SignInForm() {
+  return (
+    <div>
+      <div className="d-flex justify-content-center mb-4">
+        <p className="h4">Sign in</p>
+      </div>
+      <div className="w-100 bg-green-2 rounded rounded-4 p-5">
+        <form>
+          <div className="mb-4">
+            <input
+              className="form-control"
+              type="text"
+              placeholder="login"
+              required
+            />
+          </div>
+          <div className="mb-4">
+            <input
+              className="form-control"
+              type="password"
+              placeholder="password"
+              required
+            />
+          </div>
+          <div className="d-flex justify-content-center">
+            <button className="btn btn-primary" type="submit">
+              Submit
+            </button>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
+}
+
+export default SignInForm;
